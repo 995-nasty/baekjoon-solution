f782d17c86a278c3564e1e90540cdfcdec0b0caa
995-nasty <soonghwana714@gmail.com>
2025-10-06 03:31:44 +0900

    1927번 최소 힙 문제

---
+++ 1927.py
@@ -0,0 +1,16 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+heap = []
+
+N = int(input())
+for _ in range(N):
+    x = int(input())
+    if x == 0:
+        try:
+            print(heapq.heappop(heap))
+        except:
+            print(0)
+    else:
+        heapq.heappush(heap, x)
